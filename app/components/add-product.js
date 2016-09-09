523c34201a9458021213e0c9a67a39b50e0e103f
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-09 10:42:59 -0700

    add add-product form and fix issue with model and ensure data is working with firebase

--- app/components/add-product.js
+++ app/components/add-product.js
@@ -1,4 +1,37 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewProduct: false,
+
+  actions: {
+    productFormShow() {
+      this.set('addNewProduct', true);
+    },
+    productFormHide() {
+      this.set('addNewProduct', false);
+      this.set('image', '');
+      this.set('title', '');
+      this.set('summary', '');
+      this.set('platform', '');
+      this.set('category', '');
+    },
+    saveProd1() {
+      var params = {
+        image: this.get('image'),
+        title: this.get('title'),
+        summary: this.get('summary'),
+        platform: this.get('platform'),
+        //category here needs a hidden input on handlebar to store value from setCategory below
+        category: this.get('category')
+      };
+      //send to the action to empty/hide form on completion
+      this.send('productFormHide');
+      this.sendAction('saveProd2', params);
+    },
+    setCategory(categoryName) {
+      var newCat = this.get('model.categories').findBy('name', categoryName);
+      //category needs a hidden input on handlebar to store value from setCategory below
+      this.set('category', newCat);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model() {
-  //   return Ember.RSVP.hash({
-  //     categories: this.store.findAll('category'),
-  //     products: this.store.findAll('product'),
-  //     reviews: this.store.findAll('review')
-  //   });
-  // },
+  model() {
+    return Ember.RSVP.hash({
+      categories: this.store.findAll('category'),
+      products: this.store.findAll('product'),
+      reviews: this.store.findAll('review')
+    });
+  },
   actions: {
     saveCat5(params) {
       var newCategory = this.store.createRecord('category', params);
       newCategory.save();
-      // this.transitionTo('index');
+      this.transitionTo('index');
     },
     // saveProd5(params) {
     //   var newProduct = this.store.createRecord('product', params);
